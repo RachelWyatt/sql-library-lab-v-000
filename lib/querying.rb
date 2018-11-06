16f6bfb675c74f078688fc80177aef9a72b48f5e
Rachel Wyatt <rw126@evansville.edu>
2018-11-06 17:28:18 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -17,7 +17,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT DISCRETE series.title FROM series INNER JOIN characters ON series.id = characters.series_id WHERE characters.species = 'human';"
+  "SELECT DISC series.title FROM series INNER JOIN characters ON series.id = characters.series_id WHERE characters.species = 'human';"
 end
 
 
