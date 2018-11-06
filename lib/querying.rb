e1b9b447dea369f134c4d33a79ed0e68b3d839b6
Rachel Wyatt <rw126@evansville.edu>
2018-11-06 17:14:23 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -9,10 +9,8 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT characters.species FROM characters GROUP BY species;"
 end
-
-
 
 def select_name_and_series_subgenres_of_authors
   "Write your SQL query here"
