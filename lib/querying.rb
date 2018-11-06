fb1ed56cabadfb6301e657080e365c427231d245
Rachel Wyatt <rw126@evansville.edu>
2018-11-06 16:58:02 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,6 +1,6 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "SELECT title FROM series INNER JOIN books
-ON title.id = books.column_name;"
+  "SELECT series.title, books.year FROM series INNER JOIN books
+ON title.id = books.series_id ORDER BY year;"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
