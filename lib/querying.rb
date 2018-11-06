c7fd3f4eae4b5bd9a193712615d86e55267fcfb3
Rachel Wyatt <rw126@evansville.edu>
2018-11-06 17:34:19 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -22,5 +22,5 @@ end
 
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT characters.names FROM characters INNER JOIN character_books ON characters.id = character_books.character_id;"
+  "SELECT characters.name FROM characters INNER JOIN character_books ON characters.id = character_books.character_id;"
 end
